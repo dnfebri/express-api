4ccf8acfeb067e9ea97da655a1403a807704c823
dnfebri <dwinofebri@gmail.com>
2021-11-25 11:18:54 +0700

    set up sequelize

--- config/config.js
+++ config/config.js
@@ -1,10 +1,20 @@
-{
+require('dotenv').config();
+
+const {
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOSTNAME,
+  DB_DIALECT
+} = process.env;
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOSTNAME,
+    "dialect": DB_DIALECT
   },
   "test": {
     "username": "root",
