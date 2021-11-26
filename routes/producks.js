501407e6988715446945ef33a8171beea0c9d01f
dnfebri <dwinofebri@gmail.com>
2021-11-26 11:24:30 +0700

    express-API CRUD Sederhana

--- routes/producks.js
+++ routes/producks.js
@@ -6,7 +6,21 @@ const v = new Validator();
 
 const {Produck} = require('../models');     // memanggil model
 
-router.post('/', async (req, res) => {
+// Menampilkan semua isi tael
+router.get('/', async ( req, res ) => {
+    const produck = await Produck.findAll();
+    res.json(produck);
+});
+
+// Mencari produk berdasarkan id
+router.get('/:id', async ( req, res ) => {
+    const id = req.params.id;
+    const produck = await Produck.findByPk(id);
+    res.json(produck);
+});
+
+// Menambah Produk
+router.post('/add', async (req, res) => {
     // res.send('ini adalah post');
     const schema = {
         name: 'string',
@@ -25,5 +39,57 @@ router.post('/', async (req, res) => {
 
     res.json(produck);
 });
+
+// Update Produk
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    // res.send(id);
+
+    let produck = await Produck.findByPk(id);
+
+    if (!produck) {
+        return res.json({massage: 'Product Not Font'});
+    }
+
+    // res.send('oke');
+    // res.json(produck);
+
+    const schema = {
+        name: 'string|optional',
+        brand: 'string|optional',
+        description: 'string|optional'
+    }
+
+    const validate = v.validate(req.body, schema);
+
+    if (validate.length) {
+        return res.status(400).json(validate);
+    }
+    
+    await Produck.update(req.body, {where: {id}});
+
+    produck = await Produck.findByPk(id);
+
+    // console.log(produck);
+    res.json(produck);
+
+});
+
+router.delete('/:id', async( req, res ) => {
+    const id = req.params.id;
+
+    let produck = await Produck.findByPk(id);
+
+    if (!produck) {
+        return res.json({massage: 'Product Not Font'});
+    }
+
+    await Produck.destroy({where: {id}});
+
+    res.json({
+        message: 'Produk berhasil di hapus'
+    });
+});
+
 
 module.exports = router;
